1e7135a14c37d3f6a50b6a2d4f942a568a773842
TLP-291 <ido.maymon.44@gmail.com>
2023-01-11 10:46:13 +0200

    add polygon around circle

--- algorithmics/enemy/black_hole.py
+++ algorithmics/enemy/black_hole.py
@@ -1,5 +1,6 @@
 from algorithmics.enemy.enemy import Enemy
 from algorithmics.utils.coordinate import Coordinate
+import math
 
 
 class BlackHole(Enemy):
@@ -12,3 +13,26 @@ class BlackHole(Enemy):
         """
         self.center = center
         self.radius = radius
+
+    def get_borders(self, k: int):
+        # if k == 4:
+        #     return (Coordinate(self.center.x - self.radius, self.center.y - self.radius),
+        #             Coordinate(self.center.x + self.radius, self.center.y - self.radius),
+        #             Coordinate(self.center.x - self.radius, self.center.y + self.radius),
+        #             Coordinate(self.center.x + self.radius, self.center.y + self.radius))
+
+        # else:
+        borders = []
+        theta = math.pi/k
+        c = self.radius/math.cos(theta)
+
+        for i in range(1, 2*k, 2):
+            deg = theta*i
+            x_value = round(self.center.x + c*math.cos(deg), 2)
+            y_value = round(self.center.y + c*math.sin(deg), 2)
+            borders.append(Coordinate(x_value, y_value))
+        return borders
+
+if __name__ == '__main__':
+    bh = BlackHole(Coordinate(10,6), 5)
+    print(bh.get_borders(7))
